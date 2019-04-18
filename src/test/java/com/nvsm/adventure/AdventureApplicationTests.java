fa37e07cc07db1166f605fba5a273be116d674b6
Saurav Singh <saurav.234@gmail.com>
2019-04-18 17:57:14 +0530

    Initial commit

---
+++ src/test/java/com/nvsm/adventure/AdventureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nvsm.adventure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdventureApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
